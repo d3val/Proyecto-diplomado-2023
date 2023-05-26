db3cd80d2ae4ceb5b737d4d753de088cdf8ea52e
d3val <devalt5008@gmail.com>
2023-05-26 17:09:53 -0600

    Update SoundManager.cs

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    private SoundManager instance;
+    private static SoundManager instance;
     [SerializeField] AudioSource soundtrackAudioSource;
 
     // Start is called before the first frame update
